024b8c62c695184f562b4d15210cbd9343c1cf5b
ياسمينا عصام محمد رفعت <social2025_f168@edu.asu.edu.eg>
2026-01-27 15:48:00 +0200

    style: adjust Mobile Main Content padding for improved spacing

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -248,7 +248,7 @@ export function AdminLayout() {
             </div>
 
             {/* Mobile Main Content */}
-            <main className="lg:hidden min-h-screen pt-20">
+            <main className="lg:hidden min-h-screen pt-24">
                 <div className="p-4">
                     <Outlet />
                 </div>
